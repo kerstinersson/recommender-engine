d86bd90c68f30c647df983ee5fe7a45d2bad878d
Kerstin Ersson <kerstinersson1@gmail.com>
2018-02-26 11:08:22 +0100

    Pseudocode for file to evaluate RS

---
+++ eval.py
@@ -0,0 +1,23 @@
+# evaluates the content based recommender engine
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+# import utitily functions
+from utilities import *
+from recommender_cat import *
+
+# select 10 different wines to run the test on
+test_wines = []
+
+# run the recommender engine for these wines
+
+# calculate correctness of recommendations
+
+# precision
+
+# coverage
+
+# diversity
