29aca626296d4e4dda1fb93f284129e4131fdf69
Kerstin Ersson <kerstinersson1@gmail.com>
2018-03-01 13:23:14 +0100

    Wrote function that tests the recommender engine for six different wines and returns their recommendations

--- eval.py
+++ eval.py
@@ -2,12 +2,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 
 # import utitily functions
 from utilities import *
 from recommender_cat import *
+from recommender_cat import * 
 
-# ten wines to test
-test = ['7904', '7424',]
+
+def test_rs():
+	# ten wines to test
+	test = [7904, 7424, 7602, 77152, 5352, 2800]
+
+	artid = pd.read_csv('./artnr_artid.csv')
+	ids = []
+
+	for item in test:
+		ids.append(get_id(artid, item))
+
+	# run recommender for each wine
+	for wine in ids:
+		res = []
+		result = recommend(wine)
+		for item in result: 
+			res.append(get_nr(artid, item))
+		print(res)
+
+
+if __name__ == '__main__':
+	test_rs()
