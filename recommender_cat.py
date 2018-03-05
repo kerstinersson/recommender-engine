53f7cb7b1834559a7c67cef8f26db89d977dee78
Kerstin Ersson <kerstinersson1@gmail.com>
2018-03-05 10:21:38 +0100

    Created Class RecommenderEngine

--- recommender_cat.py
+++ recommender_cat.py
@@ -13,102 +13,64 @@ from recommender_cat import *
 HANDLE DATA
 '''
 
-def init_rs():
-	# read csv file
-	data = pd.read_csv('./rev_sysb.csv')
+class RecommenderEngine():
 
-	# clean string columns
-	features_to_clean = ['Ursprung', 'Producent', 'Typ', 'Varugrupp'] # removed Namn2 and RavarorBeskrivning for special cleaning
-	df = data.copy()
+	def __init__(self):
+		# read csv file
+		data = pd.read_csv('./data/rev_sysb.csv')
 
-	# clean data
-	for feature in features_to_clean:
-		df[feature] = data[feature].apply(clean_data)
+		# clean string columns
+		features_to_clean = ['Ursprung', 'Producent', 'Typ', 'Varugrupp'] # removed Namn2 and RavarorBeskrivning for special cleaning
+		df = data.copy()
 
-	# create bag of words and clean description box
-	df['Namn2'] = df['Namn2'].apply(bag_of_words)
-	df['Namn'] = df['Namn'].apply(bag_of_words)
-	df['RavarorBeskrivning'] = df['RavarorBeskrivning'].apply(clean_descr)
+		# clean data
+		for feature in features_to_clean:
+			df[feature] = data[feature].apply(clean_data)
 
-	# stringify data
-	df['keywords'] = df.apply(stringify, axis=1)
+		# create bag of words and clean description box
+		df['Namn2'] = df['Namn2'].apply(bag_of_words)
+		df['Namn'] = df['Namn'].apply(bag_of_words)
+		df['RavarorBeskrivning'] = df['RavarorBeskrivning'].apply(clean_descr)
 
-	count = CountVectorizer(stop_words='english')
-	count_matrix = count.fit_transform(df['keywords'])
+		# stringify data
+		df['keywords'] = df.apply(stringify, axis=1)
 
-	# similarity function
-	sim = cosine_similarity(count_matrix, count_matrix)
+		count = CountVectorizer(stop_words='english')
+		self.count_matrix = count.fit_transform(df['keywords'])
 
-	df = df.reset_index()
-	indices = pd.Series(df.index, index=df['Artikelid']).drop_duplicates()
+		# similarity function
+		self.sim = cosine_similarity(self.count_matrix, self.count_matrix)
 
-'''
-RECOMMENDER ENGINE
-'''
+		self.data = df.reset_index()
+		self.indices = pd.Series(df.index, index=df['Artikelid']).drop_duplicates()
 
-# takes article number as input and outputs a list of recommended article numbers
-def recommend(art_number):
+	# takes article number as input and outputs a list of recommended article numbers
+	def recommend(art_number, self):
 
-	# read csv file
-	data = pd.read_csv('../data/rev_sysb.csv')
+		# get variables
+		indices = self.indices
+		sim = self.sim
+		data = self.data
 
-	# clean string columns
-	features_to_clean = ['Ursprung', 'Producent', 'Typ', 'Varugrupp'] # removed Namn2 and RavarorBeskrivning for special cleaning
-	df = data.copy()
+		# get index of wine
+		idx = indices[art_number]
 
-	# clean data
-	for feature in features_to_clean:
-		df[feature] = data[feature].apply(clean_data)
+		# Get the pairwise similarity scores of all wines with that wine
+		sim_scores = list(enumerate(sim[idx]))
 
-	# create bag of words and clean description box
-	df['Namn2'] = df['Namn2'].apply(bag_of_words)
-	df['Namn'] = df['Namn'].apply(bag_of_words)
-	df['RavarorBeskrivning'] = df['RavarorBeskrivning'].apply(clean_descr)
+	    # Sort the wines based on the similarity scores
+		sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
 
-	# stringify data
-	df['keywords'] = df.apply(stringify, axis=1)
+	    # Get the scores of the 5 most similar wines
+		sim_scores = sim_scores[1:6]
 
-	count = CountVectorizer(stop_words='english')
-	count_matrix = count.fit_transform(df['keywords'])
+	    # Get the movie indices
+		wine_indices = [i[0] for i in sim_scores]
 
-	# similarity function
-	sim = cosine_similarity(count_matrix, count_matrix)
+	    # Return the top 5 most similar wines
+		return data['Artikelid'].iloc[wine_indices]
 
-	df = df.reset_index()
-	indices = pd.Series(df.index, index=df['Artikelid']).drop_duplicates()
-
-	# get index of wine
-	idx = indices[art_number]
-
-	# Get the pairwise similarity scores of all wines with that wine
-	sim_scores = list(enumerate(sim[idx]))
-
-    # Sort the wines based on the similarity scores
-	sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
-
-    # Get the scores of the 5 most similar wines
-	sim_scores = sim_scores[1:6]
-
-    # Get the movie indices
-	wine_indices = [i[0] for i in sim_scores]
-
-    # Return the top 5 most similar wines
-	return df['Artikelid'].iloc[wine_indices]
-
-'''
-MAIN
-'''
-
-# if __name__ == '__main__':
-# 	rec_input = 1125441 # ripasso della valpolicella
-# 	rec = recommend(rec_input)
-# 	name = get_info(data, rec_input)
-# 	print("Getting recommendations for " + name['Namn'])
-
-# 	for item in rec:
-# 		info = get_info(data, item)
-# 		to_print = "Namn: " + info['Namn'] + " " + info['Namn2'] + ", " + "Ursprung: " + info['Ursprung']
-# 		if not info['RavarorBeskrivning'].isnull:
-# 			to_print += ", Ravaror: " + info['RavarorBeskrivning']
-# 		print(to_print)
+if __name__ == '__main__':
+	rs = RecommenderEngine()
+	#print(recommend(617034, rs))
 
